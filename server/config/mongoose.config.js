2d7903d3d52c52f246efc6119da5a30db5efaf4a
Matt-Mckenna-26 <72038350+Matt-Mckenna-26@users.noreply.github.com>
2021-02-12 17:37:06 -0500

    deploy heroku further fixes

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv')
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${process.env.DB_NAME}`, {
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_NAME, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 })
-	.then(() => console.log(`Established a connection to the ${process.env.DB_NAME} database`))
+	.then(() => console.log(`Established a connection to the  database`))
 	.catch(err => console.log("Something went wrong when connecting to the database", err));
