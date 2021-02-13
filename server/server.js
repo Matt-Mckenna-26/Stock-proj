e639bdfdd3879d8e3ac1ce8f5f1c1a4f5f13e0fe
Matt-Mckenna-26 <72038350+Matt-Mckenna-26@users.noreply.github.com>
2021-02-13 10:44:43 -0500

    still working on heroku deployment

--- server/server.js
+++ server/server.js
@@ -4,26 +4,29 @@ const cors = require ("cors")
 const dotenv = require("dotenv")
 const cookieParser = require('cookie-parser');
 const mongoose =require("mongoose");
+const bodyParser = require('body-parser');
 
 dotenv.config();
 const PORT = process.env.PORT || 8000
 
-
+const connection = 'mongodb+srv://martimus:martimuspass@cluster0.ps40v.mongodb.net/cluster0?retryWrites=true&w=majority'
 // This will fire our mongoose.connect statement to initialize our database connection
-mongoose.connect(process.env.MONGODB_URI || process.env.DB_NAME, {
+mongoose.connect(process.env.MONGODB_URI || connection, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
+    useFindAndModify: false
 })
     .then(() => console.log(`Established a connection to the  database`))
 	.catch(err => console.log("Something went wrong when connecting to the database", err));
 
 app.use(express.json(), express.urlencoded({ extended: true }));
-app.use(cors({credentials: true, origin:'http://localhost:3000'}));
+app.use(cors({credentials: true, origin:'*'}));
 app.use(cookieParser());
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV ==="production") {
     
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, '../build')));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(_dirname, "../client", "build", "index.html"));
