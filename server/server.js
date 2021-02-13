7b15085bb4a8f56ae2bc453e277472fd21055733
Matt-Mckenna-26 <72038350+Matt-Mckenna-26@users.noreply.github.com>
2021-02-13 12:31:45 -0500

    another try

--- server/server.js
+++ server/server.js
@@ -9,6 +9,23 @@ const mongoose =require("mongoose");
 dotenv.config();
 const PORT = process.env.PORT || 8000
 
+
+app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(cors({credentials: true, origin:true}));
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'productions'){
+
+
+	app.use(express.static('../client/build'));
+}
+
+// This is where we import the users routes function from our user.routes.js file
+const AllMyUserRoutes = require("./routes/user.routes");
+const AllMyStockRoutes = require("./routes/stock.routes")
+AllMyUserRoutes(app);
+AllMyStockRoutes(app);
+
 const connection = 'mongodb+srv://martimus:martimuspass@cluster0.ps40v.mongodb.net/cluster0?retryWrites=true&w=majority'
 // This will fire our mongoose.connect statement to initialize our database connection
 mongoose.connect(process.env.MONGODB_URI || connection, {
@@ -18,20 +35,6 @@ mongoose.connect(process.env.MONGODB_URI || connection, {
 })
     .then(() => console.log(`Established a connection to the  database`))
 	.catch(err => console.log("Something went wrong when connecting to the database", err));
-
-app.use(express.json(), express.urlencoded({ extended: true }));
-app.use(cors({credentials: true, origin:true}));
-app.use(cookieParser());
-
-app.use(express.static('../client/build'));
-
-
-
-// This is where we import the users routes function from our user.routes.js file
-const AllMyUserRoutes = require("./routes/user.routes");
-const AllMyStockRoutes = require("./routes/stock.routes")
-AllMyUserRoutes(app);
-AllMyStockRoutes(app);
 
 
 app.listen(PORT, () => console.log(`The server is running and listening`));
