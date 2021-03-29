157d95d95c4f405af1d9c2ebe8233b6a7ae7f821
Matt-Mckenna-26 <72038350+Matt-Mckenna-26@users.noreply.github.com>
2021-03-29 16:27:53 -0400

    rverting

--- server/server.js
+++ server/server.js
@@ -27,10 +27,10 @@ AllMyStockRoutes(app);
 const connection = 'database connection here'
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-	app.get('/*', (req, res) => {
-			res.sendFile(path.join(__dirname, 'build', 'index.html'));
-		});
+	app.use(express.static("../client/build"));
+	app.get("/*", (req, res) => {
+		res.sendFile(path.resolve(__dirname,  "client", "build", "index.html" ))
+	});
 }
 // This will fire our mongoose.connect statement to initialize our database connection
 mongoose.connect(process.env.MONGODB_URI || connection, {
