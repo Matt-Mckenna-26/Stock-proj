249f641a9f876f168dbc0c653e263fd7f25b1949
Matt-Mckenna-26 <72038350+Matt-Mckenna-26@users.noreply.github.com>
2021-03-29 16:17:18 -0400

    fix for client side routing

--- server/server.js
+++ server/server.js
@@ -29,7 +29,10 @@ const connection = 'database connection here'
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("../client/build"));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname,  "client", "build", "index.html", "index.html/login", "index.html/register"));
+		res.sendFile(path.resolve(__dirname,  "client", "build", "index.html" ))
+	app.get('/*', function(req, res) {
+			res.sendFile(path.join(__dirname, 'build', 'index.html'));
+		});
 	});
 }
 // This will fire our mongoose.connect statement to initialize our database connection
