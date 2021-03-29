4220fab9d1052ff51e7ac50f70c9ff031fd8ff65
Matt-Mckenna-26 <72038350+Matt-Mckenna-26@users.noreply.github.com>
2021-03-29 16:46:38 -0400

    routing

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ AllMyStockRoutes(app);
 const connection = 'database connection here'
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static("../client/build"));
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 	});
